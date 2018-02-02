d6ee992b5907d0b6342ec4998e99743a4ecc31c8
Annie Chen <annie.chen@yale.edu>
2018-02-02 15:54:01 -0500

    add visualization

--- betting_agent.py
+++ betting_agent.py
@@ -1,29 +1,39 @@
-from mdp import MDP 
 import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx 
+
+from mdp import MDP 
 import pdb
 
 
-WINNING_PROB = 0.5
-NUM_WIN_CHIPS = 5
+WINNING_PROB = 0.1
+NUM_WIN_CHIPS = 10
 
 class BettingAgent(object):
 	def __init__(self, win_prob=WINNING_PROB,  num_win_chips = NUM_WIN_CHIPS):
-		s,a,t,r = self._CreateParams(win_prob, num_win_chips)
+		self.win_prob = win_prob
+		self.num_win_chips = num_win_chips
+		s,a,t,r, edge_probabilites, edge_action_labels = self._CreateParams()
+		self.s = s
+		self.a = a
+		self.t = t 
+		self.r = r
+		self.edge_probabilites = edge_probabilites
+		self.edge_action_labels = edge_action_labels
 		self.mdp = MDP(s,a, t,r)
 		if not self.mdp.Validate():
 			raise AssertionError
+		self.initDisplay()
 
-	def _CreateParams(self, win_prob, num_win_chips):
-		states = list(range(0, num_win_chips + 1)) 
-		actions = list(range(0, num_win_chips + 1)) 
+	def _CreateParams(self):
+		states = list(range(self.num_win_chips + 1)) 
+		actions = list(range(self.num_win_chips + 1)) 
 		trans = np.zeros((len(states), len(actions), len(states)))
 		rewards = np.zeros((len(actions), len(states)))
+		edge_probabilites = []
+		edge_action_labels = {}
 		for curr_state in states: # how many chips you have now
 			for action in actions: # action = # chips bet
-				# for next_state in states: # how many chips you have if you win
-				# 	# set everything to 0 first
-				# 	trans[(curr_state, action, next_state)] = 0
-				# now set the probabilities the actions lead to
 				# if not possible, go to lose
 				if action > curr_state:
 					trans[(curr_state, action, 0 )] = 1
@@ -33,20 +43,83 @@ class BettingAgent(object):
 						trans[(curr_state, action, curr_state)] = 1
 					else:
 						# if you win
-						trans[(curr_state, action, min(action + curr_state, num_win_chips))] = win_prob
+						next_state =  min(action + curr_state, self.num_win_chips)
+						trans[(curr_state, action, next_state)] = self.win_prob
+						edge_probabilites.append((curr_state, next_state, self.win_prob))
+						edge_action_labels[(curr_state, next_state)] = action
 						# if you lose 
-						trans[(curr_state, action, curr_state - action)] = 1 - win_prob
+						trans[(curr_state, action, curr_state - action)] = 1 - self.win_prob
+						edge_probabilites.append((curr_state, curr_state - action, 1 - self.win_prob))
+						edge_action_labels[(curr_state, curr_state - action)] = action
 
 		for action in actions:
 			rewards[action, 0] = -10
-			rewards[action, NUM_WIN_CHIPS] = 10
-		return states, actions, trans, rewards
+			rewards[action, self.num_win_chips] = 10
+		return states, actions, trans, rewards, edge_probabilites, edge_action_labels
 
 	def Run(self):
 		self.mdp.ValueIteration()
-		# self.mdp.BuildPolicy()
-		pdb.set_trace()
+		self.mdp.BuildPolicy()
 		self.mdp.Display(True)
+		self.Display()
+
+	def initDisplay(self):
+		self.graph = nx.MultiDiGraph()
+		self.graph.add_nodes_from(self.mdp.S)
+		self.graph_pos = nx.circular_layout(self.graph)
+		
+
+	def getMoves(self):
+		policy_mat = self.mdp.policy.transpose()
+		policy = [0 for i in self.s]
+		for i in range(len(policy)):
+			policy[i] = np.argmax(policy_mat[i])
+		print("best move at each state")
+		print(policy)
+
+		return policy
+
+	def Display(self, showpolicy=True):
+		labels = {}
+		# pdb.set_trace()
+		values = self.mdp.values.tolist()[0]
+		node_colors = [0 for i in range(self.num_win_chips+1)]
+		# create node labels
+		for state in self.mdp.S:
+			labels[state] = str(state) + " \n " + str(round(values[state],2))
+			node_colors[state] = -1 * values[state] 
+		# draw nodes
+		nx.draw_networkx_nodes(self.graph, self.graph_pos,node_color=node_colors)
+		nx.draw_networkx_labels(self.graph, self.graph_pos, labels=labels) #, labels = self.node_labels)
+
+		# choose which edge to show
+		edge_labels = None
+		edge_colors = []
+		if showpolicy:
+			moves = self.getMoves()
+			edge_list, edge_labels = [], {}
+			for i, move in enumerate(moves):
+				# if you win
+				edge_list.append((i, i + move)) #initial state, state + move
+				edge_labels[(i, i + move)] = move
+				edge_colors.append('g')
+				# if you lose
+				edge_list.append((i, max(0, i - move))) #initial state, state + move
+				edge_labels[(i, max(0, i - move))] = move
+				edge_colors.append('r')
+			self.graph.add_edges_from(edge_list)
+			# edge_colors = 'r'
+		else: # show probabilities
+			self.graph.add_weighted_edges_from(self.edge_probabilites)
+			for edge in self.graph.edges():
+				# pdb.set_trace()
+				edge_colors.append(self.edge_action_labels[edge])
+			edge_labels = self.edge_action_labels
+		
+		nx.draw_networkx_edges(self.graph, self.graph_pos, edge_color=edge_colors)
+		nx.draw_networkx_edge_labels(self.graph, self.graph_pos, edge_labels=edge_labels)
+		plt.show()
+		# TODO clear edges
 
 if __name__ == "__main__":
 	a =  BettingAgent()
