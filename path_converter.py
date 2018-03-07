28ce1482d8eda20549dd2ade318367ca2d280f36
Annie Chen <annie.chen@yale.edu>
2018-03-07 00:57:54 -0500

    path converter writes scaled path no duplicates to file

--- path_converter.py
+++ path_converter.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import csv
+from collections import OrderedDict
  
 # Define some colors
 BLACK = (0, 0, 0)
@@ -13,25 +14,83 @@ class PathConverter(object):
         # default to square
         if not height:
             height = width
+        self.h = height
+        self.w = width
         self.scale = scale
-        raw_data = self.read_data(file_name)
-        self.data = self.get_both(raw_data)
+        self.file_name = file_name
+        self.raw_data = self.read_data(file_name)
+        # data is always a list of [(x,y)] coordinates
 
+
+
+    def convert(self, remove_duplicates=True):
+        scaled = self.scale_data(self.raw_data)
+        if remove_duplicates:  
+            scaled = self.remove_duplicates(scaled)
+        self.write_data_to_file(scaled)
+            def read_data(self, file_name):
+        with open(file_name) as csvfile:
+            reader = csv.reader(csvfile)
+            data = list(reader)
+        data = [(float(x),float(y)) for (x,y) in data]
+        return data
+
+
+    # just turns from strings to nums
+    def scale_data(self, data):
+        s = float(self.scale)
+        final = [None] * len(data)
+        for i in range(len(data)):
+            x,y = data[i]
+            final[i] = [round(x/s), round(y/s)]
+        return final
+
+    def remove_duplicates(self, data):
+        final = []
+        curr = None
+        for row in data:
+            prev = curr
+            curr = row
+            if prev == curr:
+                continue
+            final.append(curr)
+        return final
+
+
+    def data_to_rects(self, data, scale = False):
+        s = self.scale
+        if scale:
+            rects = [[x*s, y*s, s,s] for (x,y) in data]
+        else:
+            rects = [[x,y,1,1] for (x,y) in data]
+        return rects
+
+    def write_data_to_file(self, data, file_name=None):
+        # default filename
+        if not file_name:
+            file_name = "scaled" + self.file_name 
+        with open(file_name, 'w+') as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
+        print("finished writing to " + file_name)
+
+    def visualize(self, data):
+
+        rects = self.data_to_rects(data)
         pygame.init()
          
         # Set the width and height of the screen [width, height]
-        screen = pygame.display.set_mode((width, height))
+        screen = pygame.display.set_mode((self.w, self.h))
          
         pygame.display.set_caption("My Game")
          
         # Loop until the user clicks the close button.
         done = False
-         
         # Used to manage how fast the screen updates
         clock = pygame.time.Clock()
         curr_square = None
         # -------- Main Program Loop -----------
-        for row in self.data:
+        for row in rects:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -67,40 +126,13 @@ class PathConverter(object):
         pygame.quit()
 
 
-    def read_data(self, file_name):
-        with open(file_name) as csvfile:
-            reader = csv.reader(csvfile)
-            data = list(reader)
-        data = [(float(x),float(y)) for (x,y) in data]
-        return data
 
-
-    # just turns from strings to nums
-    def convert_data(self, data):
-        s = float(self.scale)
-        final = [None] * len(data)
-        for i in range(len(data)):
-            x,y = data[i]
-            final[i] = [round(x/s), round(y/s)]
-        return final
-
-    def data_to_rects(self, data, scale = False):
-        s = self.scale
-        if scale:
-            rects = [[x*s, y*s, s,s] for (x,y) in data]
-        else:
-            rects = [[x,y,1,1] for (x,y) in data]
-        return rects
 
     def get_both(self, data):
         return self.data_to_rects(self.convert_data(data), scale=True)
         # self.data_to_rects(data) + self.data_to_rects(self.convert_data(data), scale=True)
 
 
-
-
-
-
-
 if __name__ == "__main__":
     c = PathConverter(file_name = "path.csv", width = 1000)
+    c.convert()
