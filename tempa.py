cc6ca4f10dee339dbea889a68aab12548d7e585d
Annie Chen <annie.chen@yale.edu>
2018-03-08 01:15:42 -0500

    fix errors

--- tempa.py
+++ tempa.py
@@ -9,15 +9,15 @@ b = GridWorldAgent(width=32,height=32,rewardValues =  {1:10})
 
 results = {}
 for filename in os.listdir("scaled_data"):
-	if filename.startswith('a')
-    with open(os.path.join("scaled_data", filename)) as csvfile:
-        reader = csv.reader(csvfile)
-        data = list(reader)
-    data = [(int(x),int(y)) for (x,y) in data]
-    action_list, path_list = b.takeListGetPath(data)
-    c = RewardGuesser(b.map.T, b.map.S, b.r, action_list, path_list, 32,32)
-    res = c.getMarginalProb()
-    results[filename] = (res, len(action_list))
+	if filename.startswith('a'):
+	    with open(os.path.join("scaled_data", filename)) as csvfile:
+	        reader = csv.reader(csvfile)
+	        data = list(reader)
+	    data = [(int(x),int(y)) for (x,y) in data]
+	    action_list, path_list = b.takeListGetPath(data)
+	    c = RewardGuesser(b.map.T, b.map.S, b.r, action_list, path_list, 32,32)
+	    res = c.getMarginalProb()
+	    results[filename] = (res, len(action_list))
 
 with open("finalresultsa", 'w+') as f:
     f.write(json.dumps(results))
