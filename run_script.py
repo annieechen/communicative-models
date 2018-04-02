0a02613f7ffe074402b11ee2a76051318c6b136b
Annie Chen <annie.chen@yale.edu>
2018-04-02 13:32:49 -0400

    redo how dict is printed

--- run_script.py
+++ run_script.py
@@ -10,6 +10,15 @@ import sys
 
 fs = []
 
+
+def to_csv(d, old_filename):
+    new_filename = os.path.join("output_data", old_filename)
+    with open(new_filename) as csvfile:
+        writer = csv.writer(csv_file, delimiter=',')
+        for key in d:
+            l = d[key]
+            writer.writerow(l)
+            
 def f(filename):
     b = GridWorldAgent(width=32,height=32,rewardValues =  {1:10})
     with open(os.path.join("d_scaled_data", filename)) as csvfile:
@@ -19,10 +28,11 @@ def f(filename):
     data = [(int(x),int(y)) for (x,y) in data]
     action_list, path_list = b.takeListGetPath(data)
     c = RewardGuesser(b.map.T, b.map.S, b.r, action_list, path_list, 32,32)
+    # this returns a dict for each csv, key from 0 to w * h, val is list from of probabilities
     res = c.getMarginalProb()
-    print(res)
-    print(len(path_list))
+    to_csv(res, filename)
     return res
+
 
 
 if __name__ == '__main__':
@@ -31,5 +41,5 @@ if __name__ == '__main__':
         fs.append(filename)
     pool = Pool(processes=25)              # start 4 worker processes
     result = pool.map(f, fs)
-    print result
+    # print result
 
