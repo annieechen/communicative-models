3d93e6e2b95967307ea56aca01c820e6c2e3eb76
Annie Chen <annie.chen@yale.edu>
2018-09-24 01:15:18 -0400

    add a reward guesser to gridworld itself

--- gridworld_agent.py
+++ gridworld_agent.py
@@ -5,206 +5,285 @@ from matplotlib import colors
 from mdp import MDP 
 from map import Map
 import pdb
-
+import random
 
 class GridWorldAgent(object):
-	def __init__(self, softmax, rewardWhenReached = False, width=10, height=10, diagonal=True, rewardValues = None):
-		"""
-		This class stores the values for the gridworld, and allows for creating paths/visualizing 
+    def __init__(self, softmax=0.01, rewardWhenReached = False, width=10, height=10, diagonal=True, rewardValues = None):
+        """
+        This class stores the values for the gridworld, and allows for creating paths/visualizing 
 
-		Args:
-			rewardWhenReached (bool): if true, creates reward matrix so reward when in state. Else, when enters state
-			width, height(bool): size of gridworld (to be passed to map)
-			diagonal (bool): whether agents can travel diagonally 
-			rewardValues (dict): (x,y) tuples to values of squares, passed to _CreateRewards
-			s (list): List of states.
-			a (list): List of actions available.
-			rewardLocations(dict): list of coordinates(1 indexed) with rewards <- should be same as reward values
-			diagonal (boolean): Determines if agents can travel diagonally.
-			t (matrix): Transition matrix. T[SO,A,SF] contains the probability that agent will go from SO to SF after taking action A.
-			policy_moves(list): move to take in each state given maxing
-		"""
-		self.map = Map(diagonal=True)
-		# fill states, transition, actions
-		self.width = width
-		self.height = height
-		self.s, self.t, self.a = self.map.BuildGridWorld(width, height, diagonal)
+        Args:
+            rewardWhenReached (bool): if true, creates reward matrix so reward when in state. Else, when enters state
+            width, height(bool): size of gridworld (to be passed to map)
+            diagonal (bool): whether agents can travel diagonally 
+            rewardValues (dict): (x,y) tuples to values of squares, passed to _CreateRewards
+            s (list): List of states.
+            a (list): List of actions available.
+            rewardLocations(dict): list of coordinates(1 indexed) with rewards <- should be same as reward values
+            diagonal (boolean): Determines if agents can travel diagonally.
+            t (matrix): Transition matrix. T[SO,A,SF] contains the probability that agent will go from SO to SF after taking action A.
+            policy_moves(list): move to take in each state given maxing
+        """
+        self.map = Map(diagonal=True)
+        # fill states, transition, actions
+        self.width = width
+        self.height = height
+        self.s, self.t, self.a = self.map.BuildGridWorld(width, height, diagonal)
 
-		self.rewardWhenReached = rewardWhenReached
-		self.rewardLocations = None
-		self.r = self._CreateRewards(rewardValues)
-		self.mdp = MDP(self.s,self.a,self.t,self.r, tau =softmax)
-		# self.policy_moves = None
-		self.policy = None
-		# pdb.set_trace()
-		if not self.mdp.Validate():
-			raise AssertionError
+        self.rewardWhenReached = rewardWhenReached
+        self.rewardLocations = None
+        self.r = self._CreateRewards(rewardValues)
+        self.mdp = MDP(self.s,self.a,self.t,self.r, tau =softmax)
+        # self.policy_moves = None
+        self.policy = None
+        # pdb.set_trace()
+        self.hasRun = False
+        if not self.mdp.Validate():
+            raise AssertionError
 
-	def _CreateRewards(self, rewardValues=False, display=False):
-		"""
-		valsDict (dictionary): maps (x,y) to reward of state
-		"""
-		# initialize to default
-		if not rewardValues:
-			rewardValues = {(1,1):10, (self.width,self.height): -10}
-		self.rewardLocations = rewardValues
-		rewards = np.full((len(self.a), len(self.s)), 0)
-		# transform coordinates to states
-		stateRewards = {}
-		for coord, val in rewardValues.iteritems():
-			if type(coord) is int:
-				state = coord
-			else:
-				state = self.map.GetRawStateNumber(coord)
-			if display:
-				print("coord: " + str(coord) + " state: " + str(state) + " val: " + str(val))
-			stateRewards[state] = val
-		# loop through all states and actions, if they reach a reward, set it
-		if self.rewardWhenReached:
-			for curr_state in self.s: 
-				for action in self.a: 
-					next_state = self.map.makeMove(curr_state, action)
-					if next_state in stateRewards:
-						rewards[action, curr_state] = stateRewards[next_state]
-		# any action they take in reward state = reward
-		else:
-			for state, val in stateRewards.iteritems():
-				for action in self.a:
-					rewards[action, state] = val
+    def _CreateRewards(self, rewardValues=False, display=False):
+        """
+        valsDict (dictionary): maps (x,y) to reward of state
+        """
+        # initialize to default
+        if not rewardValues:
+            rewardValues = {(1,1):10, (self.width,self.height): -10}
+        self.rewardLocations = rewardValues
+        rewards = np.full((len(self.a), len(self.s)), 0)
+        # transform coordinates to states
+        stateRewards = {}
+        for coord, val in rewardValues.iteritems():
+            if type(coord) is int:
+                state = coord
+            else:
+                state = self.map.GetRawStateNumber(coord)
+            if display:
+                print("coord: " + str(coord) + " state: " + str(state) + " val: " + str(val))
+            stateRewards[state] = val
+        # loop through all states and actions, if they reach a reward, set it
+        if self.rewardWhenReached:
+            for curr_state in self.s: 
+                for action in self.a: 
+                    next_state = self.map.makeMove(curr_state, action)
+                    if next_state in stateRewards:
+                        rewards[action, curr_state] = stateRewards[next_state]
+        # any action they take in reward state = reward
+        else:
+            for state, val in stateRewards.iteritems():
+                for action in self.a:
+                    rewards[action, state] = val
 
-		# alter transition matrix so that after they enter reward state, enter dead state
-		for state, val in stateRewards.iteritems():
-			# 0 everything out first
-			self.t[state, :, :] = 0
-			# now 100% chance will move to dead state
-			self.t[state,:, len(self.s) - 1] = 1
-		
-		return rewards
+        # alter transition matrix so that after they enter reward state, enter dead state
+        for state, val in stateRewards.iteritems():
+            # 0 everything out first
+            self.t[state, :, :] = 0
+            # now 100% chance will move to dead state
+            self.t[state,:, len(self.s) - 1] = 1
+        
+        return rewards
 
-	def Run(self, display=False, get_moves = True):
-		self.mdp.ValueIteration()
-		self.mdp.BuildPolicy()
-		if get_moves:
-			self.getMoves()
-		if display:
-			self.Display()
-		
+    def Run(self, display=False, get_moves =False):
+        self.mdp.ValueIteration()
+        self.mdp.BuildPolicy()
+        if get_moves:
+            self.getMoves()
+        if display:
+            self.Display()
+        self.hasRun = True
+        
 
-	def getMoves(self, print_moves = True):
-		# check to make sure ValueIteration and BuildPolicy are called
-		if not np.any(self.mdp.values):
-			self.mdp.ValueIteration()
-		if not np.any(self.mdp.policy):
-			self.mdp.BuildPolicy()
-		policy_mat = self.mdp.policy.transpose()
-		policy = [0 for i in self.s]
-		for i in range(len(policy)):
-			policy[i] = np.argmax(policy_mat[i])
-		if print_moves:
-			print("best move at each state")
-			policy_names = self.map.GetActionNames(policy)
-			# pdb.set_trace()
-			print(policy_names[:-1].reshape((self.height, self.width)))
-		self.policy = policy
-		return policy
-
-
-	def Display(self, showpolicy=False, path_list=None):
-		data = self.mdp.values[:,:-1].reshape((self.height, self.width))
-		fig, ax = plt.subplots()
-		ax.matshow(data, cmap='Greens')
-		if showpolicy:
-			for (y, x), z in np.ndenumerate(data):
-				if (x + 1, y + 1,) in path_list:
-					ax.annotate( '{:0.1f}'.format(z), xy=(x , y), xycoords='data', backgroundcolor='purple')
-				else:
-					ax.annotate( '{:0.1f}'.format(z), xy=(x , y), xycoords='data')
-		# don't change colors
-		else:
-			for (y, x), z in np.ndenumerate(data):
-				ax.annotate( '{:0.1f}'.format(z), xy=(x , y), xycoords='data')#, ha='right', va='top')
-		# highlight reward values
-		for state, val in self.rewardLocations.iteritems():
-			if type(state) is int:
-				x,y = self.map.GetCoordinates(state)
-			else:
-				x,y = state
-			if val > 0:
-				color = 'green'
-			else:
-				color = 'red'
-			ax.annotate('{:0.1f}'.format(val), xy=(x - 1, y - .7), color=color, backgroundcolor='black')
-		plt.show()
-
-	# start_coordinates = (x,y) tuple
-	def CreatePolicyPath(self, start_coordinates, max_path_length=10, print_path=False):
-		# if getMoves hasn't been called yet
-		if not self.policy:
-			self.getMoves(False)
-		curr_state = self.map.GetRawStateNumber(start_coordinates)
-		num_steps = 0
-		state_path_list = []
-		coord_path_list = []
-		action_list = []
-		while num_steps < max_path_length:
-			# move to next state
-			best_action = self.policy[curr_state]
-			state_path_list.append(curr_state)
-			coord_path_list.append(self.map.GetCoordinates(curr_state))
-			action_list.append(best_action)
-			curr_state = self.map.makeMove(curr_state, best_action)
-			num_steps += 1
-		if print_path:
-			print("path from " + str(start_coordinates))
-			for i in range(max_path_length):
-				print(str(coord_path_list[i]) + str(self.map.GetActionName(action_list[i])))
-			self.Display(showpolicy = True, path_list = coord_path_list)
-		return action_list, coord_path_list, state_path_list
-
-	def takeListGetPath(self, x_y_list):
-		state_list = []
-		action_list = []
-		curr_x, curr_y = x_y_list[0]
-		# ["L", "R", "U", "D"]
-		for x,y in x_y_list[1:]:
-			past_x, past_y = curr_x,curr_y
-			curr_x, curr_y = x,y
-			# if DR
-			if x > past_x and y > past_y:
-				action_list.append(7)
-			# DL
-			elif x < past_x and y > past_y:
-				action_list.append(6)
-			# UR
-			elif x > past_x and y < past_y:
-				action_list.append(5)
-			# UL
-			elif x < past_x and y < past_y:
-				action_list.append(4)
-			else:
-				# if down
-				if y > past_y:
-						action_list.append(3)
-				# up
-				if y < past_y:
-					action_list.append(2)
-				# R
-				if x > past_x:
-					action_list.append(1)
-				# L
-				if x < past_x:
-					action_list.append(0)
-			state_list.append(self.map.GetRawStateNumber((x,y)))
-			if len(action_list) != len(state_list):
-				print(x,y,past_x, past_y)
-		return action_list, state_list
+    def getMoves(self, print_moves = True):
+        # check to make sure ValueIteration and BuildPolicy are called
+        if not np.any(self.mdp.values):
+            self.mdp.ValueIteration()
+        if not np.any(self.mdp.policy):
+            self.mdp.BuildPolicy()
+        policy_mat = self.mdp.policy.transpose()
+        policy = [0 for i in self.s]
+        for i in range(len(policy)):
+            policy[i] = np.argmax(policy_mat[i])
+        if print_moves:
+            print("best move at each state")
+            policy_names = self.map.GetActionNames(policy)
+            # pdb.set_trace()
+            print(policy_names[:-1].reshape((self.height, self.width)))
+        self.policy = policy
+        return policy
 
 
+    def Display(self, showpolicy=False, path_list=None):
+        data = self.mdp.values[:,:-1].reshape((self.height, self.width))
+        fig, ax = plt.subplots()
+        ax.matshow(data, cmap='Greens')
+        if showpolicy:
+            for (y, x), z in np.ndenumerate(data):
+                if (x + 1, y + 1,) in path_list:
+                    ax.annotate( '{:0.1f}'.format(z), xy=(x , y), xycoords='data', backgroundcolor='purple')
+                else:
+                    ax.annotate( '{:0.1f}'.format(z), xy=(x , y), xycoords='data')
+        # don't change colors
+        else:
+            for (y, x), z in np.ndenumerate(data):
+                ax.annotate( '{:0.1f}'.format(z), xy=(x , y), xycoords='data')#, ha='right', va='top')
+        # highlight reward values
+        for state, val in self.rewardLocations.iteritems():
+            if type(state) is int:
+                x,y = self.map.GetCoordinates(state)
+            else:
+                x,y = state
+            if val > 0:
+                color = 'green'
+            else:
+                color = 'red'
+            ax.annotate('{:0.1f}'.format(val), xy=(x - 1, y - .7), color=color, backgroundcolor='black')
+        plt.show()
 
+    # start_coordinates = (x,y) tuple
+    def CreatePolicyPath(self, start_coordinates, max_path_length=10, print_path=False):
+        # if getMoves hasn't been called yet
+        if not self.policy:
+            self.getMoves(False)
+        curr_state = self.map.GetRawStateNumber(start_coordinates)
+        num_steps = 0
+        state_path_list = []
+        coord_path_list = []
+        action_list = []
+        while num_steps < max_path_length:
+            # move to next state
+            best_action = self.policy[curr_state]
+            state_path_list.append(curr_state)
+            coord_path_list.append(self.map.GetCoordinates(curr_state))
+            action_list.append(best_action)
+            curr_state = self.map.makeMove(curr_state, best_action)
+            num_steps += 1
+        if print_path:
+            print("path from " + str(start_coordinates))
+            for i in range(max_path_length):
+                print(str(coord_path_list[i]) + str(self.map.GetActionName(action_list[i])))
+            self.Display(showpolicy = True, path_list = coord_path_list)
+        return action_list, coord_path_list, state_path_list
+
+    def takeListGetPath(self, x_y_list):
+        state_list = []
+        action_list = []
+        curr_x, curr_y = x_y_list[0]
+        # ["L", "R", "U", "D"]
+        for x,y in x_y_list[1:]:
+            past_x, past_y = curr_x,curr_y
+            curr_x, curr_y = x,y
+            # if DR
+            if x > past_x and y > past_y:
+                action_list.append(7)
+            # DL
+            elif x < past_x and y > past_y:
+                action_list.append(6)
+            # UR
+            elif x > past_x and y < past_y:
+                action_list.append(5)
+            # UL
+            elif x < past_x and y < past_y:
+                action_list.append(4)
+            else:
+                # if down
+                if y > past_y:
+                        action_list.append(3)
+                # up
+                if y < past_y:
+                    action_list.append(2)
+                # R
+                if x > past_x:
+                    action_list.append(1)
+                # L
+                if x < past_x:
+                    action_list.append(0)
+            state_list.append(self.map.GetRawStateNumber((x,y)))
+            if len(action_list) != len(state_list):
+                print(x,y,past_x, past_y)
+        return action_list, state_list
+
+    def genRandomPath(self, length):
+        action_list, state_list = [], []
+        i = 0
+        action_map = ["L", "R", "U", "D", "UL", "UR", "DL", "DR"]
+        curr_x, curr_y = self.getMiddleOfMap()
+        while i < length:
+            past_x, past_y = curr_x, curr_y
+            k = random.randint(0,7)
+            action = action_map[k]
+            if 'L' in action and past_x == 0:
+                continue
+            if 'R' in action and past_x == self.width - 1:
+                continue
+            if 'U' in action and past_y == 0:
+                continue
+            if 'D' in action and past_y == self.height - 1:
+                continue
+
+            if 'L' in action:
+                curr_x -= 1
+            if 'R' in action:
+                curr_x += 1
+            if 'U' in action:
+                curr_y -= 1
+            if 'D' in action:
+                curr_y += 1
+            state_list.append(self.map.GetRawStateNumber((curr_x, curr_y)))
+            action_list.append(k)
+            i += 1
+
+        return action_list, state_list
+
+    # generates all paths of length n
+    def genAllPaths(self, n):
+        action_list, state_list = [], []
+        i = 0
+        action_map = ["L", "R", "U", "D", "UL", "UR", "DL", "DR"]
+        action_lists = permutations(range(8), n)
+
+
+        curr_x, curr_y = self.getMiddleOfMap()
+        while i < length:
+            past_x, past_y = curr_x, curr_y
+            k = random.randint(0,7)
+            action = action_map[k]
+            if 'L' in action and past_x == 0:
+                continue
+            if 'R' in action and past_x == self.width - 1:
+                continue
+            if 'U' in action and past_y == 0:
+                continue
+            if 'D' in action and past_y == self.height - 1:
+                continue
+
+            if 'L' in action:
+                curr_x -= 1
+            if 'R' in action:
+                curr_x += 1
+            if 'U' in action:
+                curr_y -= 1
+            if 'D' in action:
+                curr_y += 1
+            state_list.append(self.map.GetRawStateNumber((curr_x, curr_y)))
+            action_list.append(k)
+            i += 1
+
+    def getMiddleOfMap(self):
+        return self.width//2, self.height//2
+
+    def getLikelihoodOfPath(self, action_list, path_list):
+        if not self.hasRun:
+            self.Run()
+        probabilities = self.mdp.policy
+        prob_list = []
+        product_probabilities = 1
+        for i in range(len(action_list)):
+            action, state = action_list[i], path_list[i]
+            product_probabilities *= (probabilities[action,state])
+        return product_probabilities
 
 if __name__ == "__main__":
-	a =  GridWorldAgent(width=3,height=4, rewardValues = {(2,4):10, (3,1): -20})
-	a.Run(display = True)
+    a =  GridWorldAgent(width=3,height=4, rewardValues = {(2,4):10, (3,1): -20})
+    a.Run(display = True)
 
 
 
