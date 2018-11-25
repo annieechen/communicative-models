5409916ad4548eaafb8474656a2807bfcde529b2
Annie Chen <annie.chen@yale.edu>
2018-11-24 20:39:38 -0500

    make script run with command line arguments

--- path_converter.py
+++ path_converter.py
@@ -11,26 +11,23 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
 class PathConverter(object):
-    def __init__(self, file_name, width=100, height = None, scale = 10):
-        # default to square
-        if not height:
-            height = width
-        self.h = height
-        self.w = width
+    def __init__(self, file_name, scale, include_diagonals):
         self.scale = scale
         self.file_name = file_name
         self.raw_data = self.read_data(file_name)
+        self.include_diagonals= include_diagonals
         # data is always a list of [(x,y)] coordinates
 
-    def convert(self, remove_duplicates=True, write_data=True, diagonals=False):
+    def convert(self, remove_duplicates=True, write_data=True):
         scaled = self.scale_data(self.raw_data)
         if remove_duplicates:
             scaled = self.remove_duplicates(scaled)
             # scaled = self.replace_with_diagonals(scaled)
-        if not diagonals:
+        if not self.include_diagonals:
             scaled = self.remove_diagonals(scaled)
+        action_list = self.convert_coordstates_to_actions(scaled)
         if write_data:
-            self.write_data_to_file(scaled)
+            self.write_data_to_file(action_list)
 
     def read_data(self, file_name):
         with open(file_name) as csvfile:
@@ -41,7 +38,7 @@ class PathConverter(object):
 
 
     # just turns from strings to nums
-    def scale_data(self, data, remove_extra=True):
+    def scale_data(self, data, remove_extra=False):
         s = float(self.scale)
         final = [None] * len(data)
         for i in range(len(data)):
@@ -83,14 +80,14 @@ class PathConverter(object):
         return final
 
 
-    def convert_coordstates_to_actions(self, coordlist, include_diagonals=False):
+    def convert_coordstates_to_actions(self, coordlist):
         action_list = []
         curr_x, curr_y = coordlist[0]
         # ["L", "R", "U", "D"]
         for x,y in coordlist[1:]:
             past_x, past_y = curr_x,curr_y
             curr_x, curr_y = x,y
-            if include_diagonals:
+            if self.nclude_diagonals:
                 #if DR
                 if x > past_x and y > past_y:
                     action_list.append(7)
@@ -162,7 +159,7 @@ class PathConverter(object):
             file_name =  file_prefix + self.file_name
         else:
             file_name = self.file_name
-        with open(os.path.join("no_diag_30", os.path.basename(file_name)), 'w+') as f:
+        with open(os.path.join(outputdirectory, os.path.basename(file_name)), 'w+') as f:
             writer = csv.writer(f)
             writer.writerows(data)
         print("finished writing to " + file_name)
@@ -216,10 +213,20 @@ class PathConverter(object):
 
 
 if __name__ == "__main__":
-    # c = PathConverter(file_name = "data/inin.csv", width = 1000)
-    # # c.convert(write_data=False)
-    # c.visualize()
-    for file in os.listdir("data"):
-        print (file)
-        c = PathConverter(file_name = os.path.join("data", file), width =30)
-        c.convert(write_data=False)
+    if len(sys.argv) < 4:
+        print("usage: path_converter [datainputdirectory] [actionlistresultsdirectory] [scalefactor] [-d if diagnoals")
+        exit(1)
+    datainputdirectory = sys.argv[1]
+    scalefactor = sys.argv[2]
+    # this one is a global so write_data_to_file can use it
+    global outputdirectory
+    outputdirectory = sys.argv[3]
+    if len(sys.argv) == 5 and sys.argv[4] == "-d":
+        include_diagonals = True
+    else:
+        include_diagonals = False
+
+    for file in os.listdir(datainputdirectory):
+        print ("Processing %s" % file)
+        c = PathConverter(file_name=os.path.join(datainputdirectory, file),scale=scalefactor, include_diagonals=include_diagonals)
+        c.convert()
